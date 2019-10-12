104c46515d0d55d616546206cbfd827de916a970
Vaibhav Yadaram <yadaram.vaibhav@gmail.com>
2019-10-12 18:02:17 -0400

    add button

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,14 @@ function App() {
     return body;
   };
 
+  const handleClick = async () => {
+    const response = await fetch("/handle_click", { method: "POST" }).then(
+      res => {
+        setData(res.json().express);
+      }
+    );
+  };
+
   useEffect(() => {
     callBackendAPI()
       .then(res => {
@@ -27,18 +35,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={handleClick}></button>
         <p>{data}</p>
       </header>
     </div>
