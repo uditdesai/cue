45674f369a7be2244a0ac5f7c39d282252bff404
Lynn <lynnkim2000@gmail.com>
2019-10-12 16:15:14 -0400

    updates

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
-app.listen(3000, () => {
-  console.log("server is running");
+app.get("/express_backend", (req, res) => {
+  res.send({ express: "BACKEND" });
 });
